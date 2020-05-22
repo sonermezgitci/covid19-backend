6a6ec216baaa78dc558d4e8dc8a69006d4034ac3
Soner Mezgitci <mezgitci9@gmail.com>
2020-05-22 02:28:18 -0400

    nested attributes updated for user_symptoms_controller.rb, user_symptom.rb

--- app/controllers/user_symptoms_controller.rb
+++ app/controllers/user_symptoms_controller.rb
@@ -11,20 +11,29 @@ class UserSymptomsController < ApplicationController
     render json: user_symptom
     
     end 
-
+    
+    
+    def new 
+        @user_sypmtom = UserSymptom.new
+        @user_symptom.user.new
+        @user_symptom.symptom.new
+    end 
     
     def create 
-        user_symptom  = UserSymptom.new(user_symptoms_params)
+        user_symptom  = @user.UserSymptom.new(user_symptoms_params)
+        user_symptom  = @symptom.UserSymptom.new(user_symptoms_params)
+
         if user_symptoms.save
             render json: user_symptoms
-            # json:userSymptoms, status: :accepted 
+            
         else 
             render json: {errors: userSymptoms.error.full_messages}, status: :unprocessible_entity 
         end 
     end 
+
     
     private 
     def user_symptoms_params
-        params.require(:user_symptoms).permit(:id, :user_id, :symptom_id)
+        params.require(:user_symptoms).permit(:id, :user_id, :symptom_id,user_attributes:[:name, :lastname,:gender,:age,:symptom_id],symptom_attributes: [:fever, :cough, :breath, :throat, :nose, :other, :user_id])
     end 
 end
--- app/models/user_symptom.rb
+++ app/models/user_symptom.rb
@@ -1,4 +1,7 @@
 class UserSymptom < ApplicationRecord
     belongs_to :user
     belongs_to :symptom
+
+    accepts_nested_attributes_for :user
+    accepts_nested_attributes_for :symptom
 end
