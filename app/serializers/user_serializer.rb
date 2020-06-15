295e20a8bde663502615ef15737b7e4106e5222f
Soner Mezgitci <mezgitci9@gmail.com>
2020-06-14 20:35:38 -0400

    UPDATED

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :name, :lastname, :age, :gender #if you use symptoms and quarantines here as instance it will noy use serializer
+  attributes :id,:name, :lastname, :age, :gender #if you use symptoms and quarantines here as instance it will noy use serializer
   has_many :symptoms #this one uses the Serializer for symptoms
   has_many :quarantines  #this one uses the Serializer for quarantines
 end
