704eaca701c242a356129cf6a56477b777cc1189
Soner Mezgitci <mezgitci9@gmail.com>
2020-06-17 23:21:33 -0400

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-resources :quarantines, only: [:index, :create]
-resources :symptoms, only: [:index, :create]
-resources :users, only: [:index, :create]
+resources :quarantines, only: [:index, :show, :create]
+resources :symptoms, only: [:index, :show, :create]
+resources :users, only: [:index, :show, :create]
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
