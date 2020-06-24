9a0854dbdac48389534c76c14f615b4f49f53126
Soner Mezgitci <mezgitci9@gmail.com>
2020-06-23 20:01:44 -0400

    QuarantinesControler updated

--- app/controllers/quarantines_controller.rb
+++ app/controllers/quarantines_controller.rb
@@ -1,2 +1,12 @@
 class QuarantinesController < ApplicationController
-end
+    def index
+        @quarantines = Quarantine.all
+        render json: @quarantines
+    end 
+
+ 
+def show 
+    @quarantine = Quarantine.find(params[:id])
+    render json: @quarantine
+ end 
+ end
